6d8c09376f5288c9a8e2a6af9aaccbfd512555a4
aandnota <myers.alex@gmail.com>
2017-02-01 15:54:32 -0600

    spinning update

--- js/master.js
+++ js/master.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $("#button").click(function(event) {
     $(this).toggleClass('boxbig'),
-    $('ul.menu>li').toggle()
+    $('ul.menu>li').toggle(),
+    $('ul.menu>li').toggleClass('spin')
   });
 });
