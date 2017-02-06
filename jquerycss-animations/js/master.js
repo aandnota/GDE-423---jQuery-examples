be157f83cab85e92fe075ee9f0335a3dbf31c161
Aandnota <myers.alex@gmail.com>
2017-02-06 14:04:16 -0600

    updates

--- jquerycss-animations/js/master.js
+++ jquerycss-animations/js/master.js
@@ -5,3 +5,8 @@ $('.trigger').on('click', function(){
 $('.trigger2').on('click', function(){
   $(this).toggleClass('clicked');
 })
+
+$('.trigger3').on('click', function(){
+  $(this).toggleClass('is-paused');
+  /* Act on the event */
+});
